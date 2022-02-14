6498cc2bff81f4a0ba6657b2160091e3f85037e6
jacobhsu <jacob.hsu.tw@gmail.com>
2022-02-14 16:06:27 +0800

    feat: react-router-dom

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Prompt
+} from 'react-router-dom'
+
+function AppHello() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function AppInner () {
+  return (<>
+    <Prompt
+      message={(location, action) => {
+        if (action === 'POP') return alert('onPopHistory')
+        return true
+    }}/>
+    <Switch>
+      <Route path="/wallet">
+        {AppHello()}
+      </Route>
+      <Route path="/">
+        <Redirect to="/wallet"/>
+      </Route>
+    </Switch>
+    </>)
+}
+
+// handles all the providers so that we can use provider hooks inside of AppInner
+function App() {
+  return (
+    <Router>
+       <AppInner/>
+    </Router>
+  )
+}
+
+export default App;
